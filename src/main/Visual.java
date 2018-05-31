2b3e9c1f365ba0c731e840bbf8f63d630838d08c
s-linben <s-linben@bsd405.org>
2018-05-30 19:15:28 -0700

    Visual Library Added
    Added the visual library

---
+++ src/main/Visual.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.util.ArrayList;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;						// IDK what this is for
+import javafx.scene.Group;						// IDK what this is for
+import javafx.scene.Scene;						// IDK what this is for
+import javafx.scene.canvas.Canvas;				// JavaFX canvas class
+import javafx.scene.canvas.GraphicsContext;		// IDK what this is for
+import javafx.scene.image.Image;				// JavaFX importing pictures and stuff
+import javafx.scene.paint.Color;				// This is for color
+import javafx.scene.text.Font;					// Remove this once you delete the practice stuff, useless
+import javafx.scene.text.FontWeight;			// Remove this as well, because don't need automated text
+import javafx.scene.input.KeyEvent;
+
+public class Visual extends Application {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	@Override
+	public void start(Stage stage) {
+		
+		stage.setTitle("The Trip");
+		
+	}
+	
+}
